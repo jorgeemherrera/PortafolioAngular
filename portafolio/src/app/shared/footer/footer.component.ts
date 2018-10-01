3156ebf072cdcee20ba5979dc238e1cc2f1d8eb3
Jorge Eliécer Muñoz Herrera <jorge.herrera@quipux.com>
2018-09-30 19:42:19 -0500

    Template listo sin rutas

---
+++ portafolio/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  /**
+   * Propiedad año actual
+   */
+  anio: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
